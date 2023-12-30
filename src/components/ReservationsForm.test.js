5632cd010930d457c2166f88eb5ff2f2b5ed4b36
Godfrey Offeh <103786881+Pyppersama@users.noreply.github.com>
2023-12-30 12:53:49 +0100

    Add files via upload

---
+++ src/components/ReservationsForm.test.js
@@ -0,0 +1,60 @@
+import { render, screen } from "@testing-library/react";
+import ReservationsForm from "./ReservationsForm";
+
+test('Renders the Reservation form Heading', () => {
+    render(<ReservationsForm />);
+    const headingElement = screen.getByText('Reservations');
+    expect(headingElement).toBeInTheDocument();
+});
+
+describe("BookingForm", () => {
+    test("Renders labels and fields", () => {
+      render(
+        <BookingForm
+          availableTimes={{
+            times: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"],
+          }}
+          dispatch={jest.fn((action) => action)}
+        />
+      );
+      // Date label and field
+        const choseDateLabel = screen.getByText("Choose date");
+        expect(choseDateLabel).toBeInTheDocument();
+        const choseDateField = screen.getByTestId("res-date");
+        expect(choseDateField).toBeInTheDocument();
+
+        const choseTimeLabel = screen.getByText("Choose time");
+        expect(choseTimeLabel).toBeInTheDocument();
+        const choseTimeField = screen.getByTestId("res-time");
+        expect(choseTimeField).toBeInTheDocument();
+
+        const numberGuestLabel = screen.getByText("Number of guests");
+        expect(numberGuestLabel).toBeInTheDocument();
+        const numberGuestField = screen.getByTestId("guests");
+        expect(numberGuestField).toBeInTheDocument();
+
+        const occasionLabel = screen.getByText("Occasion");
+        expect(occasionLabel).toBeInTheDocument();
+        const occasionField = screen.getByTestId("occasion");
+        expect(occasionField).toBeInTheDocument();
+        const submitButton = screen.getByDisplayValue("Make Your reservations");
+        expect(submitButton).toBeInTheDocument();
+        });
+
+        test("initializeTimes returns the correct expected value", () => {
+            const today = new Date();
+            const initialState = initializeTimes();
+            const expectedResult = { times: fetchAPI(today) };
+            expect(initialState).toEqual(expectedResult);
+        });
+
+        test("updateTimes returns the same state", () => {
+            const state = {
+            times: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"],
+            };
+            const action = { type: "SOME_ACTION" };
+            const newState = updateTimes(state, action);
+            expect(newState).toEqual(state);
+        });
+
+});
---
+++ src/pages/ConfirmedBookingPage.js
@@ -0,0 +1,13 @@
+import ConfirmedBooking from "../components/ConfirmedBooking";
+import Header from "../components/Header";
+
+const ConfirmedBookingPage = () => {
+  return 
+    <>
+    <Header />
+    <ConfirmedBooking />;
+    </>
+  
+};
+
+export default ConfirmedBookingPage;
